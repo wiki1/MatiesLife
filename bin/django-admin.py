06d5e38b18d478d3b94e543d817134389d98173c
hendrik <hendrikmarais@hotmail.com>
2015-05-25 12:21:34 +0200

    remove directory bin

--- bin/django-admin.py
+++
@@ -1,6 +0,0 @@
-#!/home/orielle/Documents/GroupProject/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'Django==1.9.dev20150523193830','django-admin.py'
-__requires__ = 'Django==1.9.dev20150523193830'
-__import__('pkg_resources').require('Django==1.9.dev20150523193830')
-__file__ = '/home/orielle/Documents/GroupProject/django-trunk/django/bin/django-admin.py'
-exec(compile(open(__file__).read(), __file__, 'exec'))
