c4a9fdfda3f5db49a5e8ff05b61363f107b1b7da
craigNyazika <17089379@sun.ac.za>
2015-05-28 00:05:11 +0200

    removed unecessary files

--- login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-	url(r'^$', views.loginView, name='login'),
-	url(r'^/loggedIn/$', views.loggedIn, name='loggedIn')
-]
--- postingArticles/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Article, Content
-# Register your models here.
-admin.site.register(Article)
-admin.site.register(Content)
