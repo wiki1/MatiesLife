bdee02246a38d8b115ab359344c934e8cbc8ee9c
hendrik <hendrikmarais@hotmail.com>
2015-05-28 12:38:22 +0200

    zip file

--- search functionality added/ourSite/wikiApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Users, Requests, Articles, Content
-
-# Register your models here.
-
-admin.site.register(Users)
-admin.site.register(Requests)
-admin.site.register(Articles)
-admin.site.register(Content)
